c2cb06fb2d707ac1d5b60270b0221db3ad008fa0
Inna <shvartz_i@mail.ru>
2021-12-24 00:37:15 +0300

    added boolean polufabrikat

---
+++ Task18_Boolean/Program.cs
@@ -0,0 +1,18 @@
+﻿// Проверить истинность ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool X = true;
+bool Y = false;
+bool Statement1 (bool Х, bool Y)
+ {
+    bool result1 = !(X|Y);
+    return result1;
+ }
+bool Statement2 (bool Х, bool Y)
+ {
+    bool result2 = !X & !Y;
+    return result2;
+ }
+   //bool S1 = Statement1(X,Y)
+if (Statement1(X,Y) == Statement2(X,Y))
+Console.WriteLine ("Ура"); 
+
