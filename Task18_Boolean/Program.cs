7ef275aa230c52e031da0aeb9a09a16c372620e2
Inna <shvartz_i@mail.ru>
2021-12-25 02:36:04 +0300

    упрощение Task18_Boolean

--- Task18_Boolean/Program.cs
+++ Task18_Boolean/Program.cs
@@ -9,21 +9,7 @@ Console.Write("Введите значение Y: ");
 string y = Console.ReadLine();
 bool Y = Boolean.Parse(y);
 
-bool Statement1 (bool Х, bool Y)
- {
-    bool result1 = !(X|Y);
-    return result1;
- }
-bool Statement2 (bool Х, bool Y)
- {
-    bool result2 = !X & !Y;
-    return result2;
- }
-   
-if (Statement1(X,Y) == Statement2(X,Y))
+if ((!(X|Y)) == (!X & !Y))
    Console.WriteLine ("Высказывание истинно"); 
 else 
    Console.WriteLine ("Высказывание ложно");
-
-   
-
