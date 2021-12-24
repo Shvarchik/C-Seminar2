8b7bbc0cb75e2608146eb9244f463ecd9d2fe242
Inna <shvartz_i@mail.ru>
2021-12-24 20:45:00 +0300

    в методах изменила Х на А

--- Task18_1_BoolArrCycl/Program.cs
+++ Task18_1_BoolArrCycl/Program.cs
@@ -1,30 +1,26 @@
-﻿// Проверить истинность ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+﻿// Проверить истинность ¬(A ⋁ Y) = ¬A ⋀ ¬Y
 // Замороченный вариант, с массивами и циклами
 
-bool X = false; // не понимаю, зачем, но пока я не объявила X и Y и не присвоила им значения, не работало.
-bool Y = false; // Так программа работает, но пишет, что переменной Y присвоено значение, но оно ни разу 
-                // не использовано. Дык ведь и Х не использовано, но пока не было задано, не работало
-
-bool Statement1 (bool Х, bool Y)
+bool Statement1 (bool A, bool Y)
  {
-    bool result1 = !(X|Y);
+    bool result1 = !(A|Y);
     return result1;
  }
-bool Statement2 (bool Х, bool Y)
+bool Statement2 (bool A, bool Y)
  {
-    bool result2 = !X & !Y;
+    bool result2 = !A & !Y;
     return result2;
  }
 
 Boolean [] arX = {false,true};
-Boolean [] arY = {false,true};
 Boolean [] Check = new Boolean [4];
 int Xindex, Yindex, CheckIndex = 0;
+
 for (Xindex=0; Xindex <=1; Xindex++)
     {
         for (Yindex=0; Yindex <=1; Yindex++)
             {
-                if (Statement1(arX[Xindex],arY[Yindex]) == Statement2(arX[Xindex],arY[Yindex]))
+                if (Statement1(arX[Xindex],arX[Yindex]) == Statement2(arX[Xindex],arX[Yindex]))
                     {
                         Check[CheckIndex] = true;
                         CheckIndex++;
