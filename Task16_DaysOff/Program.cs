0295b38534da43e001afdfaa2dff9f03c884b6c8
Inna <shvartz_i@mail.ru>
2021-12-25 03:59:46 +0300

    Task16 added

---
+++ Task16_DaysOff/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Write("Введите номер дня недели: ");
+string Number = Console.ReadLine();
+int num = int.Parse(Number);
+string[]  Week = {"понедельник","вторник","среда","четверг","пятница","суббота","воскресенье"};
+
+if (num <= 1 && num > 8)
+    Console.WriteLine ("В неделе только 7 дней, дня {0} нет", num);
+    else if (Week[num-1] == "cуббота" || Week[num-1] == "воскресенье")
+            Console.WriteLine ("{0} - выходной день", Week[num-1]);
+         else 
+            Console.WriteLine ("{0} - рабочий день", Week[num-1]);
+
