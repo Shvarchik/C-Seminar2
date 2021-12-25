a2c327b42281ef4dec25c8c7befc86490bb1aa0a
Inna <shvartz_i@mail.ru>
2021-12-25 03:25:50 +0300

    Task17 added

---
+++ Task17_Chek_Squares/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число:");
+string number1 = Console.ReadLine();
+int num1 = int.Parse(number1);
+Console.Write("Введите второе число:");
+string number2 = Console.ReadLine();
+int num2 = int.Parse(number2);
+
+if (num1 == num2 * num2)
+    Console.WriteLine("число {0} является квадратом {1}", num1, num2);
+
+else if (num2 == num1 * num1)
+       Console.WriteLine("число {0} является квадратом {1}", num2, num1);
+     else
+       Console.WriteLine("ни одно из чисел не является квадратом другого");
