42320d6996f8127696533025f17e879c02a9945d
Inna <shvartz_i@mail.ru>
2021-12-25 04:52:24 +0300

    Task21 added

---
+++ Task21_Palindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа проверяет пятизначное число на палиндром.
+
+Console.Write ("Введите пятизначное число:");
+string Number = Console.ReadLine();
+int Num = int.Parse (Number);
+
+int n1 = Num / 10000;
+int n5 = Num % 10;
+int n4 = (Num % 100 - Num % 10) / 10;
+int n2 = (Num / 1000) % 10;
+  
+if (n1 == n5 && n2 == n4)
+    Console.WriteLine ("Число {0} является палиндромом", Num);
+else 
+    Console.WriteLine ("Число {0} не является палиндромом", Num);
+
